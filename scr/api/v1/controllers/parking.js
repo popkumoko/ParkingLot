29ccb04f1fd469217f9086a176ecaece4944ff73
Wichittra Iam-itsara <wichittra.iamitsara@gmail.com>
2020-04-09 23:20:13 +0700

    create API

--- scr/api/v1/controllers/parking.js
+++ scr/api/v1/controllers/parking.js
@@ -1,10 +1,62 @@
 'use strict'
 const httpStatus = require('http-status');
+const { parkingLotService, parkCarService } = require('../../../services');
 
-const getParking = async (req, res) => {
-  res.status(httpStatus.OK).send('Hello');
+const createParkingLot = async (req, res) => {
+  // {
+  //   parkingSmall: {
+  //     pricePerHour: 20,
+  //     slotStart: 1,
+  //     slotEnd: 30,
+  //   },
+  //   parkingMedium: {
+  //     pricePerHour: 30,
+  //     slotStart: 31,
+  //     slotEnd: 60,
+  //   },
+  //   parkingLarge: {
+  //     pricePerHour: 40,
+  //     slotStart: 61,
+  //     slotEnd: 100,
+  //   }
+  // }
+  const payload = { ...req.body };
+
+  const duplicateSlot = await parkingLotService.createParkingLot(payload);
+  if(duplicateSlot.length != 0) {
+    res.status(httpStatus.OK).send('have duplicate slot ' + duplicateSlot.toString());
+  } else {
+    res.status(httpStatus.OK).send();
+  }
 };
 
+const createParkCar = async (req, res) => {
+  const payload = { ...req.body };
+  const result = await parkCarService.getFreeSlot(payload.numberPlate, payload.type);
+  res.status(httpStatus.OK).send(result);
+};
+
+const leaveParkCar = async (req, res) => {
+  const payload = { ...req.body };
+  const result = await parkCarService.leaveParkCar(payload.slotCode);
+  res.status(httpStatus.OK).send(result.toString());
+};
+
+const getFreeSlot = async (req, res) => {
+  const result = await parkingLotService.getFreeSlot();
+  res.status(httpStatus.OK).send(result);
+};
+
+const getAllNumberPlate = async (req, res) => {
+  const { carSize }  = { ...req.query };
+  const result = await parkCarService.getAllNumberPlateByCarSize(carSize);
+  res.status(httpStatus.OK).send(result);
+}
+
 module.exports = {
-  getParking,
+  createParkingLot,
+  createParkCar,
+  leaveParkCar,
+  getFreeSlot,
+  getAllNumberPlate
 };
--- scr/api/v1/routes/parking.js
+++ scr/api/v1/routes/parking.js
@@ -3,6 +3,11 @@ const parkingController = require('../controllers/parking')
 
 const router = express.Router();
 
-router.get('/', parkingController.getParking);
+router.post('/createParkingLot', parkingController.createParkingLot);
+router.post('/parkCar', parkingController.createParkCar);
+router.post('/leaveSlot', parkingController.leaveParkCar);
+router.get('/getFreeSlot', parkingController.getFreeSlot);
+router.get('/getAllNumberPlate', parkingController.getAllNumberPlate);
+
 
 module.exports = router;
